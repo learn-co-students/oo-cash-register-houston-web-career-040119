605a4d1aee816228dd171ac15b58877a216bbc79
minhl337 <imnotminh@gmail.com>
2019-04-04 16:16:53 -0500

    o fails

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
 
+class CashRegister
+
+    attr_reader :discount, :items
+    attr_accessor :total, :last_transaction
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items=[]
+    end
+
+    
+
+    def add_item(title,price,quantity=1)   
+        self.total += price * quantity
+        quantity.times do 
+            @items.push(title)
+        end
+        self.last_transaction = price * quantity
+    end
+
+    def apply_discount
+        if @discount !=0
+        self.total = @total - (@total * @discount / 100)
+        "After the discount, the total comes to $#{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
+end
